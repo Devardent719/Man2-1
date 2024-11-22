d93c23bc84624cd44de2cf8ac26e63bdc9ccabdf
Artem Boronin <110157528+Devardent719@users.noreply.github.com>
2024-11-22 10:04:31 +0300

    Create Man21 and method Talk

---
+++ Man21.java
@@ -0,0 +1,54 @@
+
+import java.util.Random;
+
+public class Man21{
+    public static void main(String[] args){
+        Man man = new Man("Mark");
+    }
+}
+class Man{
+    public Man(String _name){
+        Name = _name;
+        isLife = true;
+        
+        Age = rand.nextInt(35) + 15;
+        Health = rand.nextInt(90) + 10;
+        System.out.println(Age + " " + Health);
+        Talk();
+    }
+
+    private final Random rand = new Random();
+
+    private String Name;
+    private int Age;
+    private int Health;
+    private boolean isLife;
+
+    public void Talk(){
+        int randTalk = rand.nextInt(1, 4);
+        String str = "";
+
+        switch(randTalk){
+            case 1:
+                str = "Привет, меня зовут " + Name + ", рад познакомиться";
+                break;
+            case 2:
+                str = "Мне " + Age + ". А тебе?";
+                break;
+            case 3:
+                if (Health > 50) 
+                    str = "Да я здоров как бык!"; 
+                else 
+                    str = "Со здоровьем у меня хреново, дожить бы до " + (Age + 10);
+            break;
+        }
+    }
+    public void Go(){
+        
+    }
+    public boolean isAlive(){
+
+
+        return isLife;
+    }
+}
